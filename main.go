0ac4a2c6ccd2fe0693bacbc1761ba953cfe519a6
Iriko0606 <eriko.i0606@gmail.com>
2022-06-16 16:49:59 +0900

    user取得

--- main.go
+++ main.go
@@ -13,13 +13,16 @@ func main() {
 
 	// log.Println("test")
 
-	fmt.Println(models.Db)
+	// fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.PassWord = "pass"
+	// u := &models.User{}
+	// u.Name = "test"
+	// u.Email = "test@example.com"
+	// u.PassWord = "pass"
+	// fmt.Println(u)
+
+	// u.CreateUser()
+
+	u, _ := models.GetUser(1)
 	fmt.Println(u)
-
-	u.CreateUser()
 }
